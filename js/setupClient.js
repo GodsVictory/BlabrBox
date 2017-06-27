98f218462699a3bd26d0d6175c7649603e636857
GodsVictory <thegodsvictory@gmail.com>
2017-06-26 23:44:27 -0500

    simulation setup

--- js/setupClient.js
+++ js/setupClient.js
@@ -9,7 +9,17 @@ function setupClient() {
   client.on('message', handleChat);
   client.on('connected', function() {
     var channel = Qurl.create().query('c');
-    if (channel)
+    if (channel == 'simulateChat') {
+      setInterval(function() {
+        handleChat('', '', '0000');
+      }, 500);
+      setInterval(function() {
+        handleChat('', '', '11');
+      }, 500);
+      setInterval(function() {
+        handleChat('', '', Math.round((Math.random() * 100)).toString());
+      }, 250);
+    } else
       client.join(channel);
   });
   client.connect();
