b145098da791073cd6846e556933952eec6e0247
GodsVictory <thegodsvictory@gmail.com>
2018-01-23 10:50:21 -0600

    Revert "Merge branch 'gh-pages' of https://github.com/GodsVictory/BlabrBox into gh-pages"
    This reverts commit 58e5d796a4ad0af767a00211ea29ea97bcb5c659, reversing
    changes made to 9522fd7ebcb062e7fab5229a22104c7cf965dba9.

--- js/handleChat.js
+++ js/handleChat.js
@@ -1,5 +1,9 @@
 function handleChat(channel, user, message, self) {
   if (!document.hidden)
+<<<<<<< HEAD
     if (message.length <= length)
+=======
+    if (length > -1 && message.length <= length)
+>>>>>>> parent of 58e5d79... Merge branch 'gh-pages' of https://github.com/GodsVictory/BlabrBox into gh-pages
       newChat.push(message);
 }
