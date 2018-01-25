6957cbb997d5a32fb7c00d332396f9f4ab9f6320
GodsVictory <thegodsvictory@gmail.com>
2018-01-25 00:43:18 -0600

    fix font size

--- js/chat.js
+++ js/chat.js
@@ -1,7 +1,7 @@
 function Chat(message) {
   var style = new PIXI.TextStyle({
     fontFamily: 'Fredoka One',
-    fontSize: window.innerWidth * window.innerHeight * .00004,
+    fontSize: window.innerHeight * .1,
     align: 'center',
     fill: '#ffffff',
     stroke: '#000000',
