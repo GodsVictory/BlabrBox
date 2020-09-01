bf61f633218c8838b79cbc00f06947bb4d9a4190
Reid Lindsey <thegodsvictory@gmail.com>
2020-09-01 15:48:56 -0500

    remove old assets

--- js/chat.js
+++ js/chat.js
@@ -30,8 +30,8 @@ function Chat(message) {
       //else
       //  url = "assets/emotes/ffz/" + memes[messageArray[i]] + ".png";
       var emote = new PIXI.Sprite.fromImage(url);
-      emote.width = height / meme.h * meme.w; // * .575;
-      emote.height = height / meme.h * meme.h; // * .575;
+      emote.width = height / meme.h | 28 * meme.w | 28; // * .575;
+      emote.height = height / meme.h | 28 * meme.h | 28; // * .575;
       if (i > 0)
         emote.x = this.container.getBounds().width;
       emote.anchor.set(0, .5);
