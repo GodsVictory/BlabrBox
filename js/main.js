70b37015629bb481ee66ec46c37282201c91d893
Reid Lindsey <reidandkat@gmail.com>
2020-09-01 18:37:45 +0000

    Update index.html, js/lib/lzma-worker.min.js, js/main.js files

--- js/main.js
+++ js/main.js
@@ -61,6 +61,7 @@ function load() {
 
   loader.add('emotes', 'assets/emotes.json');
   loader.add('badwords', 'assets/badwords.json');
+  var my_lzma = new LZMA("lib/lzma_worker.js");
   loader.once('complete', function(loader, resources) {
     memes = resources.emotes.data;
     badwords = resources.badwords.data;
